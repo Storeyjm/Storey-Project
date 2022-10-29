2d3a994e792e75e43945c69d7a9488aeb7eb10e9
Storeyjm <85558096+Storeyjm@users.noreply.github.com>
2022-10-29 15:56:48 +0100

    29.10.22 - Enemy movement 2

--- ArrowQuest.py
+++ ArrowQuest.py
@@ -1,5 +1,6 @@
 import imghdr
 from pickle import FALSE, TRUE
+from tkinter import Y
 import pygame
 import random
 import math
@@ -15,10 +16,11 @@ GREY =  (163,163,163)
 GREEN = (151,244,139)
 BROWN = (165, 42, 42)
 GOLD =  (255,215,  0)
+BLUE =  ( 60,250,255)
 
 
 map_1 =[[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-        [1,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
         [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
         [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
         [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
@@ -67,7 +69,7 @@ map_1 =[[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
         [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]]
 
 map_2 =[[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-        [1,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
         [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
         [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
         [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
@@ -177,12 +179,20 @@ class Player(pygame.sprite.Sprite):
             self.rect.y = self.rect.y + self.Vspeed
         #End if
     #End function
+
+    def get_x(self):
+        return self.rect.x
+    # end get
+
+    def get_y(self):
+        return self.rect.y
+    # end get
 #End Class
 
 # - Define class Enemy - Which is a sprite
 class Enemy(pygame.sprite.Sprite):
     #Define constructor for Enemy
-    def __init__(self, color, width, height, speed, x_pos, y_pos, facing_pos):
+    def __init__(self, color, width, height, speed, x_pos, y_pos, facing_pos, chasing):
         #Set speed of the sprite
         self.start_y = y_pos
         self.start_x = x_pos
@@ -201,39 +211,116 @@ class Enemy(pygame.sprite.Sprite):
         self.x_dist = 150
         self.y_dist = 100
         self.speed = speed
+        self.x_speed = 0
+        self.y_speed = 0
+        #Seperate x and y speeds for enemy movement
+        self.curr_x = 0
+        self.curr_y = 0
+        # chasing the player when set to 1
+        self.chasing = chasing
+        self.enemy_flag = 0
     #End Procedure
 
+    def chase_player(self):
+        play_x = my_game.my_player.get_x()
+        play_y = my_game.my_player.get_y()
+        
+        x_diff = self.rect.x - play_x
+        y_diff = self.rect.y - play_y
+
+        if x_diff ==0:
+            x_diff = 1
+        if y_diff == 0:
+            y_diff = 1
+
+        
+        if abs(y_diff) < abs(x_diff):
+            move_x = abs(x_diff) // abs(y_diff)
+            move_y = 1
+        else:
+            move_y = abs(y_diff) // abs(x_diff)
+            move_x = 1
+        #end if
+
+        if x_diff > 0:
+            move_x = move_x * -1
+        #end if
+
+        if y_diff > 0:
+            move_y = move_y * -1
+        #end if
+       
+        return move_x, move_y
+        
+    #end chase player function
+
     def update(self):
-        if self.facing == 0 or 1:
-            if self.rect.y >= self.start_y and self.rect.y < self.start_y + self.y_dist:
-                self.rect.y = self.rect.y + self.speed
-            else:
-                if self.facing == 1:
-                    self.speed = random.randrange(2,5) * -1
-                    self.facing = 0
+        if self.chasing == 0:
+            # 0 moving up and 1 is moving down 
+            if self.facing == 0 or self.facing ==1:
+                if self.rect.y >= self.start_y and self.rect.y <= self.start_y + self.y_dist:
                     self.rect.y = self.rect.y + self.speed
-                elif self.facing == 0:
-                    self.facing = 1
-                    self.speed = random.randrange(2,5)
-                    self.y_dist = random.randrange(100,150)
-                    self.rect.y = self.rect.y + self.speed
-                #endif
-        elif self.facing == 2 or 3:
-            if self.rect.x >= self.start_x and self.rect.x < self.start_x + self.x_dist:
-                self.rect.x = self.rect.x + self.speed
-            else:
-                if self.facing == 2:
-                    self.facing = 3
-                    self.speed = random.randrange(2,5) * -1
+                else:
+                    # change direction from down to up
+                    if self.facing == 1:
+                        self.speed = random.randrange(2,5) * -1
+                        self.facing = 0
+                        self.rect.y = self.rect.y + self.speed
+                        # change direction from up to down
+                    elif self.facing == 0:
+                        self.facing = 1
+                        self.speed = random.randrange(2,5)
+                        self.y_dist = random.randrange(100,150)
+                        self.rect.y = self.rect.y + self.speed
+                    #endif
+            elif self.facing == 2 or self.facing ==3:
+                if self.rect.x >= self.start_x and self.rect.x <= self.start_x + self.x_dist:
                     self.rect.x = self.rect.x + self.speed
-                elif self.facing == 3:
-                    self.facing = 2
-                    self.speed = random.randrange(2,5)
-                    self.x_dist = random.randrange(150,200)
-                    self.rect.x = self.rect.x + self.speed
+                else:
+                    if self.facing == 2:
+                        self.facing = 3
+                        self.speed = random.randrange(2,5) * -1
+                        self.rect.x = self.rect.x + self.speed
+                    elif self.facing == 3:
+                        self.facing = 2
+                        self.speed = random.randrange(2,5)
+                        self.x_dist = random.randrange(150,200)
+                        self.rect.x = self.rect.x + self.speed
+                    #endif
                 #endif
             #endif
-        #endif
+        else:
+            if self.enemy_flag == 0:
+                self.x_speed, self.y_speed = self.chase_player()
+                self.curr_x = abs(self.x_speed)
+                self.curr_y = abs(self.y_speed)
+                #print (self.curr_x, self.curr_y)
+                self.enemy_flag = self.enemy_flag + 1
+            elif self.enemy_flag < 5:
+                if self.curr_x == 0 and self.curr_y == 0:
+                    self.curr_x = abs(self.x_speed)
+                    self.curr_y = abs(self.y_speed)
+                else:
+                    if self.curr_x >= self.curr_y:
+                        if self.x_speed < 0:
+                            self.rect.x -= 2
+                        else:
+                            self.rect.x += 2
+                        #end if
+                        self.curr_x -= 1
+                    else:
+                        if self.y_speed < 0:
+                            self.rect.y -= 2
+                        else:
+                            self.rect.y += 2
+                        #end if
+                        self.curr_y -= 1
+                    # end if
+                self.enemy_flag = self.enemy_flag + 1
+            else:
+                self.enemy_flag = 0
+            #endif
+        #end if
     #End Procedure
 #End Class
 
@@ -320,10 +407,10 @@ class Game():
 
         # Create enemies
         # List of enemy starting positions and facing directions
-        enemy_positions =[[580,80,1],[460,20,1],[420,160,1],[840,420,1],[80,360,3]]
+        enemy_positions =[[580,80,1,1],[460,20,1,0],[420,160,1,0],[840,420,1,1],[80,360,3,0]]
         self.enemy_group = pygame.sprite.Group()
         for pos in range (enemy_range):
-            my_enemy = Enemy(RED,20,20,3,enemy_positions[pos][0], enemy_positions[pos][1], enemy_positions[pos][2])
+            my_enemy = Enemy(RED,20,20,3,enemy_positions[pos][0], enemy_positions[pos][1], enemy_positions[pos][2], enemy_positions[pos][3])
             self.enemy_group.add(my_enemy)
             self.all_sprites_group.add(my_enemy)
         #Next pos
@@ -344,6 +431,10 @@ class Game():
                     self.all_sprites_group.add(my_obstacle)
                 elif current_map[x][y] ==3:
                     my_obstacle = Obstacle(BROWN, 20, 60, y*20, x*20)
+                    self.obstacle_group.add(my_obstacle)
+                    self.all_sprites_group.add(my_obstacle)
+                elif current_map[x][y] ==4:
+                    my_obstacle = Obstacle(BLUE, 20, 20, y*20, x*20)
                     self.obstacle_group.add(my_obstacle)
                     self.all_sprites_group.add(my_obstacle)
                 elif current_map[x][y] ==5:
